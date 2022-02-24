2bfd6f36e62329862f709e9e31635f582034f814
include-minimaltools <gabrielortizamador@gmail.com>
2022-02-24 10:13:11 -0600

    Fix bug - CSS imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom'
 import { App } from './App';
+import './style/bootstrap.css';
+import './style/maicons.css';
+import './style/mobster.css';
+import './vendor/animate/animate.css';
+import './vendor/owl-carousel/css/owl.carousel.min.css';
 
 ReactDOM.render(
     <App/>,
